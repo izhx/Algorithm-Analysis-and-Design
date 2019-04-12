80bc2eb84beaa6097a32ac84da7da9b697b12938
izhx <izhx404@gmail.com>
2019-04-12 23:50:21 +0800

    change para to const &.

--- Assignments/ex1.cpp
+++ Assignments/ex1.cpp
@@ -35,7 +35,7 @@ void Tournament(vector<vector<int>> &table, int n) {
  * @return k-th smallest element
  */
 template<typename T>
-T SelectKth(vector<T> A, int k) {
+T SelectKth(vector<T> &A, int k) {
     if (A.size() < 75) {
         sort(A.begin(), A.end());
         return A[k];
@@ -64,12 +64,12 @@ T SelectKth(vector<T> A, int k) {
 struct Point {
     double x, y;
 
-    double d2(Point p) {
+    double d2(const Point &p) {
         return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
     }
 };
 
-double EfficientClosetPair(vector<Point> P) {
+double EfficientClosetPair(vector<Point> &P) {
     double d = 2147483647;
     int n = P.size(), i = 0, j = 1;
     if (n <= 3) {  // brute force
